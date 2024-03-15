41d7a729577acce52aa546e17bee7cf5d020cabd
Funina-qwq <1296189522@qq.com>
2024-03-15 23:34:31 +0800

    ignore

--- train.py
+++ train.py
@@ -131,7 +131,7 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--data_path',default="C:\\Users\\lenovo\\Desktop\\DCGAN\\Data\\dataset2-master\\images\\TRAIN")          #'./data/dogs-cats/train/'
+    parser.add_argument('--data_path',default="C:/Users/lenovo/Desktop/DCGAN/Data/dataset2-master/images/TRAIN")          #'./data/dogs-cats/train/'
     parser.add_argument('--epochs',default=1000)
     parser.add_argument('--batch_size',default=32)
     parser.add_argument('--lr',default=1e-5)
@@ -139,7 +139,7 @@ if __name__ == '__main__':
     parser.add_argument('--saved_img',default='result')
     parser.add_argument('--num_classes',default=4)
     parser.add_argument('--seed',default=0)
-    parser.add_argument('--val_data_path',default="C:\\Users\\lenovo\\Desktop\\DCGAN\\Data\\dataset2-master\\images\\TEST")
+    parser.add_argument('--val_data_path',default="C:/Users/lenovo/Desktop/DCGAN/Data/dataset2-master/images/TEST")
 
     args = parser.parse_args()
 
