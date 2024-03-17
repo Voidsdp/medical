bf5f577188496cc57d99eec4e102bbbd9afb8284
Funina-qwq <1296189522@qq.com>
2024-03-17 19:11:01 +0800

    v1.0.2

--- configs/data.py
+++ configs/data.py
@@ -26,14 +26,3 @@ class data:
                     transforms.Normalize(mean,std),
          ]),
     }              
-
-class model:
-      nz = 100 #噪声维度
-      ndf = 64
-      ngf = 64
-      nc = 4 #分类种类
-
-      checkpoint = {
-           'discriminator': 'checkpoint/discriminator.pth',
-           'generator': 'checkpoint/generator.pth',
-      }
